e7d28a67ed9e30a7ed3339de45e557bb84cf8c69
chasehammock <chasehammock@gmail.com>
2017-09-27 14:03:28 -0500

    added my files for the server name generator working

---
+++ src/util/Input.java
@@ -0,0 +1,67 @@
+package util;
+
+import java.util.Scanner;
+
+public class Input {
+  private Scanner scan;
+
+  public Input() {
+    scan = new Scanner(System.in);
+  }
+
+  public String getString() {
+    String userString = scan.nextLine();
+    return userString;
+  }
+
+  public boolean yesNo() {
+    String userInput = this.scan.nextLine();
+    return userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes");
+  }
+
+  public int getInt() {
+    if (this.scan.hasNextInt()) {
+      int input = this.scan.nextInt();
+      scan.nextLine();
+      return input;
+    } else {
+      System.out.println("That's not an integer! Try again.");
+      scan.nextLine();
+      return getInt();
+    }
+
+  }
+
+  public int getInt(int min, int max) {
+    int userInput = getInt();
+
+    if (userInput < min || userInput > max) {
+      System.out.println(userInput + " is not between " + min + " and " + max);
+      return getInt(min, max);
+    }
+
+    return userInput;
+  }
+
+  public double getDouble() {
+    if (this.scan.hasNextDouble()) {
+      return this.scan.nextDouble();
+    } else {
+      System.out.println("That's not an double! Try again.");
+      scan.nextLine();
+      return getDouble();
+    }
+  }
+
+  public double getDouble(double min, double max) {
+    double userInput = getDouble();
+
+    if (userInput < min || userInput > max) {
+      System.out.println(userInput + " is not between " + min + " and " + max);
+      return getDouble(min, max);
+    }
+
+    return userInput;
+  }
+
+}
