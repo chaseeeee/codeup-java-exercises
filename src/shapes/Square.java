a86bb97132f0f84fb18c494de74da892436cb323
chasehammock <chasehammock@gmail.com>
2017-09-28 12:06:29 -0500

    added everything from the shapes exercise that's working

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,4 +1,16 @@
 package shapes;
 
-public class Square {
+public class Square extends Rectangle{
+
+public Square(int side){
+  super(side, side);
 }
+}
+
+
+/*
+
+        Create a class named Square, also inside of shapes, that extends Rectangle. Square should define a constructor that accepts one argument, side, and calls the parent's constructor to set both the length and width to the value of side.
+
+
+        */
